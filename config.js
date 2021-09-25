5b1a7455c9334614f1457d54f34452a813920d3d
Iverick <iverick091@gmail.com>
2021-09-25 16:10:03 +0300

    Changed address to MONGODB_URI in the config file.

--- config.js
+++ config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  PORT: process.env.PORT || 3000,
-  mongoUri: process.env.MONGODB_URI ||
-    process.env.MONGO_HOST ||
-    'mongodb://' + (process.env.IP || 'localhost') + ':' +
-    (process.env.MONGO_PORT || '27017') +
-    '/vue_test'
+  mongoUri: process.env.MONGODB_URI,
+  PORT: process.env.PORT || 3000
 }
