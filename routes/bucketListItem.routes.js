cc55142532e86c8063d09d233e807899608b2a40
Iverick <iverick091@gmail.com>
2021-09-25 15:53:22 +0300

    Tried to rename reference to models file.

--- routes/bucketListItem.routes.js
+++ routes/bucketListItem.routes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const BucketListItem = require("../models/BucketListItem.models")
+const BucketListItem = require("../models/bucketListItem.models.js")
 
 const router = express.Router()
 
